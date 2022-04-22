dfa0fb575919c3049220042a1174adcf541c5731
handou2 <2864466519@qq.com>
2022-04-22 16:41:26 +0800

    opt:header and footer

---
+++ src/custom.d.ts
@@ -0,0 +1,8 @@
+declare module "*.scss" {
+  const scss: { [key: string]: string };
+  export default scss;
+}
+declare module "*.css" {
+  const css: { [key: string]: string };
+  export default css;
+}
