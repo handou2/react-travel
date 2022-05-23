8a4d778884b4f010e8fe3f2f703310c7777c6869
handou2 <2864466519@qq.com>
2022-05-23 16:46:51 +0800

    fix:readme

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -2,7 +2,3 @@ declare module "*.scss" {
   const scss: { [key: string]: string };
   export default scss;
 }
-declare module "*.css" {
-  const css: { [key: string]: string };
-  export default css;
-}
